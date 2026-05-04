bd7dddde131c69aa40df6551383481a481d213df
Evan Binder <47007288+evnb@users.noreply.github.com>
2026-05-04 14:46:52 -0400

    Created Github Actions deploy.yml workflow. (And added base path configuration to Svelte setup)

--- svelte.config.js
+++ svelte.config.js
@@ -1,12 +1,15 @@
 import adapter from '@sveltejs/adapter-static';
 
+const basePath = process.env.BASE_PATH ?? '';
+
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	compilerOptions: {
 		runes: ({ filename }) => (filename.split(/[/\\]/).includes('node_modules') ? undefined : true)
 	},
 	kit: {
-		adapter: adapter()
+		adapter: adapter(),
+		paths: { base: basePath }
 	}
 };
 
