e3a52669372969271a16c69cb4d65df347549982
Evan Binder <47007288+evnb@users.noreply.github.com>
2026-05-04 15:58:20 -0400

    Update page title and description for Sun Exposure Calculator

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,4 @@
-<h1>Welcome to SvelteKit</h1>
+<h1 class="h1">Sun Exposure Calculator 🌞</h1>
+<h4 class="h4">Calculate sun exposure on a trip, and see which side of the bus / train you should sit on ➡️🚇⬅️</h4>
 <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+
