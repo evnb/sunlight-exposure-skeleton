a58ab6acf83f1643615b821211e2f6d25eb537be
Evan Binder <47007288+evnb@users.noreply.github.com>
2026-05-04 22:09:51 -0400

    Update description and Add Word Joiner charcter between ➡️🚇⬅️ emoji

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -21,7 +21,7 @@
 <div class="flex flex-col gap-6">
 	<div>
 		<h1 class="h1">Sun Exposure Calculator 🌞</h1>
-		<h4 class="h4">Calculate sun exposure on a trip, and see which side of the bus / train you should sit on ➡️🚇⬅️</h4>
+		<h4 class="h4">Calculate sun exposure on a trip, and see which side of the bus / train you should sit on to avoid the sun ➡️⁠🚇⁠⬅️</h4>
 	</div>
 
 	<div class="flex flex-col gap-4">
